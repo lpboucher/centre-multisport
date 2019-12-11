1f05665fa56da41e6b56317fb994a8f416f75966
LP Boucher <lp_boucher@hotmail.com>
2019-12-11 18:31:51 +0100

    add simple org description

--- src/components/OrgDescription/index.js
+++ src/components/OrgDescription/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import Container from '../../structural/Container';
 import Heading from '../../generic/Heading';
+import MobileDisplay from '../../structural/MobileWrapper';
 
 const OrgDesc = () => (
-    <Container background="black">
-        <Heading color="white">Le Centre Multisport par excellence sur la rive-nord.</Heading>
-    </Container>
+    <MobileDisplay showMobile>
+        <Container background="primaryDark">
+            <Heading color="white">Le Centre Multisport par excellence sur la rive-nord.</Heading>
+        </Container>
+    </MobileDisplay>
 )
 
 export default OrgDesc;
