d13ad612976a2e34332faab1aa84a17a6cf7ef8b
LP Boucher <lp_boucher@hotmail.com>
2019-12-11 18:32:04 +0100

    add home page

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Helmet from 'react-helmet';
 
 import Layout from '../structural/Layout';
 import Hero from '../components/Hero';
+import OfferList from '../components/OfferList';
 import OrgDescription from '../components/OrgDescription';
+import News from '../components/News';
+import Contact from '../components/Contact';
+import Gallery from '../components/Gallery';
 
-const MainPage = ({ data, location }) => (
+const MainPage = ({ data, location, pageContext }) => (
   <Layout data={data} location={location}>
+    <Helmet>
+      <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+      <script src="https://kwes.io/js/kwes.js"></script>
+    </Helmet>
     <Hero />
+    <OfferList locale={pageContext.locale}/>
     <OrgDescription />
+    <News />
+    <Contact />
+    <Gallery />
   </Layout>
 );
 
