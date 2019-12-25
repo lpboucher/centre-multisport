683a5dd51f3cd7ae9ed7e7b4c6585fff346ce002
LP Boucher <lp_boucher@hotmail.com>
2019-12-25 17:40:27 +0100

    adding map placeholder

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -5,12 +5,14 @@ import Helmet from 'react-helmet'
 import Layout from '../structural/Layout';
 import Hero from '../components/Hero';
 import Contact from '../components/Contact';
+import Map from '../components/Map';
 
 const ContactPage = ({data, location, pageContext}) => (
     <Layout data={data} location={location}>
         <Helmet><script src="https://kwes.io/js/kwes.js"></script></Helmet>
         <Hero page={pageContext.template} />
         <Contact />
+        <Map />
     </Layout>
 );
 
