df185eddcbe2e32c05d16f5f089d186ab2912000
LP Boucher <lp_boucher@hotmail.com>
2020-01-16 20:34:27 +0100

    hide kwes disclaimer

--- src/structural/forms/FormWrapper.js
+++ src/structural/forms/FormWrapper.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
 const FormWrapper = styled.div`
+    margin-bottom: -37px;
     & form {
         border-right: ${({withSeparator}) => withSeparator ? 'solid 1px white' : ''};
+    }
+
+    & .kw-credit a {
+        opacity: 0;
     }
 `;
 
