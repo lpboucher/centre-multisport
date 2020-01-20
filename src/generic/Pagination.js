8a915700bf6f81f1604614854d9c09fae6f12fb5
LP Boucher <lp_boucher@hotmail.com>
2020-01-20 16:01:04 +0100

    correct pagination to work for categories also

--- src/generic/Pagination.js
+++ src/generic/Pagination.js
@@ -3,11 +3,12 @@ import React from 'react';
 import Flex from '../structural/Flex';
 import MenuLink from '../generic/MenuLink';
 
-const Pagination = ({ pathPrefix, currentPage, numPages }) => {
+const Pagination = ({ category, pathPrefix, currentPage, numPages }) => {
+    const urlStart = category ? `${pathPrefix}/${category.toLowerCase()}` : pathPrefix;
     const isFirst = currentPage === 1
     const isLast = currentPage === numPages
     const prevPage = currentPage - 1 === 1 ? pathPrefix : `${pathPrefix}/${(currentPage - 1).toString()}`
-    const nextPage = `${pathPrefix}/${(currentPage + 1).toString()}`
+    const nextPage = `${urlStart}/${(currentPage + 1).toString()}`
     return (
         <Flex justifyBetween>
             {!isFirst ? (
