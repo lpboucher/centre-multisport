71225ca242c95fef8dcc76c51fd69dc75cc45d7c
LP Boucher <lp_boucher@hotmail.com>
2020-01-20 16:00:44 +0100

    logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Link from 'gatsby-link';
 import {useIntl} from 'react-intl';
 
+import ImageWrapper from '../../generic/ImageWrapper';
+import logo from '../../assets/image/CDCSR_logotype-coul_seul (1)-1.png'
+
 const Logo = () => {
     const { locale } = useIntl();
     return (
-        <h1>
-            <Link to={`/${locale}`}>Centre Multisport</Link>
-        </h1>
+        <Link to={`/${locale}`}>
+            <ImageWrapper height="66px" width="93px" margin="-25px 0">
+                <img src={logo} alt="logo"/>
+            </ImageWrapper>
+        </Link>
     );
 };
 
