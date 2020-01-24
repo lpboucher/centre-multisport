da55e7034c4f0adfc3bba47ae1ba750666786923
LP Boucher <lp_boucher@hotmail.com>
2020-01-24 11:15:15 +0100

    line-height on overlay

--- src/components/Hero/Overlay.js
+++ src/components/Hero/Overlay.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Container from '../../structural/Container';
 import Heading from '../../generic/Heading';
 
+const WithLineH = styled(Heading)`
+    line-height: 150%;
+`
+
 const Overlay = ({text}) => {
     return (
         <Container absolute background="transparent" top="40%">
-            <Heading huge color="primaryLight">{text}</Heading>
+            <WithLineH huge color="primaryLight">{text}</WithLineH>
         </Container>
     );
 };
