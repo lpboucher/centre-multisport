86f2e8072a14cbd9b0364f02156d7266f664c51f
LP Boucher <lp_boucher@hotmail.com>
2020-01-24 11:14:35 +0100

    temp about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,12 +7,15 @@ import Text from '../../generic/Text';
 const About = () => (
     <Container background="primaryDark">
         <Heading color="primaryLight">Le Centre Multisport par excellence sur la rive-nord.</Heading>
-        <Text>
+        <Text spaced justify>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel consectetur magna, quis malesuada erat. Nunc dolor turpis, scelerisque eu metus id, sagittis vestibulum enim. Sed nec ullamcorper risus. Aenean eu sem magna. Aliquam porttitor nibh at bibendum hendrerit. Suspendisse quis efficitur est, sit amet elementum ipsum. Aenean hendrerit, turpis ut elementum iaculis, diam neque elementum tortor, eget commodo eros est ac nisl.
 
 Vivamus ultrices maximus quam elementum dignissim. Aliquam iaculis ultrices odio, ut pellentesque mi fringilla ac. Donec a turpis luctus, vulputate nisl eget, hendrerit urna. Vestibulum vehicula mattis euismod. Aliquam erat volutpat. Praesent dignissim egestas risus non mattis. Maecenas tellus urna, sollicitudin in sem facilisis, vestibulum pellentesque enim. Integer aliquam malesuada rhoncus. Morbi felis elit, sodales vel nulla ut, eleifend interdum risus. Quisque eleifend pulvinar nisi ac posuere. Nullam mollis facilisis magna, eget ultrices lorem rutrum at. Aliquam ac lobortis arcu. Praesent feugiat enim purus, in luctus tortor aliquam quis. Maecenas lacinia dui vel commodo euismod. Mauris sapien tortor, hendrerit at convallis vitae, aliquet vitae orci.
 
-Aliquam aliquam lorem sed odio dictum placerat. Proin iaculis tincidunt dui, in interdum sem sodales at. Etiam euismod nec justo quis vulputate. Integer ligula leo, facilisis sed metus at, tempor tempus nisl. Etiam aliquet lectus at orci commodo, quis laoreet urna feugiat. Suspendisse elit nulla, commodo vitae metus id, consequat rhoncus ipsum. Nullam eu sem dui.
+Aliquam aliquam lorem sed odio dictum placerat.
+        </Text>
+        <Text spaced justify>
+        Proin iaculis tincidunt dui, in interdum sem sodales at. Etiam euismod nec justo quis vulputate. Integer ligula leo, facilisis sed metus at, tempor tempus nisl. Etiam aliquet lectus at orci commodo, quis laoreet urna feugiat. Suspendisse elit nulla, commodo vitae metus id, consequat rhoncus ipsum. Nullam eu sem dui.
 
 Fusce laoreet maximus mi, vel volutpat nunc rutrum a. Fusce pretium mauris eu orci gravida, sit amet aliquam justo tempor. Phasellus et maximus dolor. Aliquam sed libero magna. Pellentesque rhoncus tortor eu ipsum pulvinar, et pretium ipsum aliquet. Donec laoreet velit vel elementum pellentesque. Aenean lobortis condimentum nisl eget commodo. Cras in felis pharetra, posuere massa vitae, venenatis erat.
 
