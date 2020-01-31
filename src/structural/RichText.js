dc06b235fb2b54cf3f2895913c1733dd0702542c
LP Boucher <lp_boucher@hotmail.com>
2020-01-31 17:58:19 +0100

    add rendering of embedded assets in rich text

--- src/structural/RichText.js
+++ src/structural/RichText.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
 
 import Container from './Container';
 import Heading from '../generic/Heading';
 import Text from '../generic/Text';
 
+const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node) => {
+        const { url } = node.data.target.fields.file['fr'];
+        console.log()
+        return <img style={{width: '100%'}} src={url} />
+      }
+    }
+  };
+
 const RichText = ({title, content}) => {
     return (
         <Container background="primaryDark">
-            <Heading color="primaryLight">{title}</Heading>
-            <Text justify large spaced>{documentToReactComponents(content.json)}</Text>
+            <Heading large bold color="primaryLight">{title}</Heading>
+            <Text justify large>{documentToReactComponents(content.json, options)}</Text>
         </Container>
     )
 }
