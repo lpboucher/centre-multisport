3f63dd1f45f63b01d2badae1bbfa9923751619b2
LP Boucher <lp_boucher@hotmail.com>
2020-01-31 17:57:26 +0100

    remove hook, query moved to template level

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import {useIntl} from 'react-intl';
 
 import RichText from '../../structural/RichText';
 
-import { useAbout } from "../../hooks/use-about";
-
 const About = ({title, content}) => {
-    const { locale } = useIntl();
-    const about = useAbout({node_locale: [locale], slug: 'about'});
     return (
         <RichText 
             title={title}
