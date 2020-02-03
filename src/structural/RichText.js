c848ee2d09f810651d11bf208da490ce8ac46ec9
LP Boucher <lp_boucher@hotmail.com>
2020-02-03 17:35:58 +0100

    output img from rich text

--- src/structural/RichText.js
+++ src/structural/RichText.js
@@ -1,25 +1,44 @@
 import React from 'react';
+import Link from 'gatsby-link';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BLOCKS } from '@contentful/rich-text-types';
+import { FormattedDate, useIntl } from 'react-intl';
 
 import Container from './Container';
 import Heading from '../generic/Heading';
 import Text from '../generic/Text';
 
-const options = {
+const imgStyle = {
+  width: '100%',
+  height: '100%',
+  maxWidth: '960px',
+  maxHeight: '400px',
+  objectFit: 'cover',
+  marginBottom: '40px'
+}
+
+const createOptions = (locale) => {
+  return {
     renderNode: {
       [BLOCKS.EMBEDDED_ASSET]: (node) => {
         const { url } = node.data.target.fields.file['fr'];
-        console.log()
-        return <img style={{width: '100%'}} src={url} />
+        return <Link to={`/${locale}/gallery`}><img style={imgStyle} src={url} /></Link>
       }
     }
-  };
+  }
+}
 
-const RichText = ({title, content}) => {
+const RichText = ({title, content, date}) => {
+    const { locale } = useIntl();
+    const options = createOptions(locale);
     return (
         <Container background="primaryDark">
             <Heading large bold color="primaryLight">{title}</Heading>
+            {date &&
+            <Text justify large>
+              <FormattedDate value={date} day="numeric" month="long" year="numeric"/>
+            </Text>
+            }
             <Text justify large>{documentToReactComponents(content.json, options)}</Text>
         </Container>
     )
