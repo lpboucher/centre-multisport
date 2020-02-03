cf11da2bb487ca0ad473485caeacc25aadb05b26
LP Boucher <lp_boucher@hotmail.com>
2020-02-03 17:34:56 +0100

    responsive

--- src/generic/MenuLink.js
+++ src/generic/MenuLink.js
@@ -1,14 +1,21 @@
 import styled from 'styled-components';
 import Link from 'gatsby-link';
+import { device } from '../theme';
 
 const MenuLink = styled(Link)`
+    font-size: 80%;
     text-transform: uppercase;
     font-weight: bold;
-    padding: 0 1rem;
+    padding: 0 0.75rem;
     color: ${({color, theme}) => color ? theme[color] : theme['primaryLight']};
 
     &:hover {
         color: ${({theme}) => theme['primaryHighlight']};;
+    }
+
+    @media ${device.laptop} {
+        font-size: 100%;
+        padding: 0 1rem;
     }
 `;
 
