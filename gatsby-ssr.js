06a41f39ad66b5494fde5d26c91793ca77e5a03a
LP Boucher <lp_boucher@hotmail.com>
2020-02-11 17:55:50 +0100

    fix errors in SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,12 +4,12 @@ exports.onRenderBody = ({
   setPostBodyComponents,
 }) => {
   setHeadComponents([
-    <link rel="preconnect" href="https://kwes.io" crossorigin />,
-    <link rel="preconnect" href="https://fonts.googleapis.com/" crossorigin />
+    <link key="kwes-head" rel="preconnect" href="https://kwes.io" crossOrigin="true" />,
+    <link key="google-font-head" rel="preconnect" href="https://fonts.googleapis.com/" crossOrigin="true" />
   ]);
   setPostBodyComponents([
-    <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />, 
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />,
-    <script async src="https://kwes.io/js/kwes.js" />
+    <link key="slick-body" rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />, 
+    <link key="slick-theme-body" rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />,
+    <script key="kwes-body" async src="https://kwes.io/js/kwes.js" />
   ]);
 }
