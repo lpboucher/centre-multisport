4ec5afb4f7559753f44f4a616b6c983d982a24df
LP Boucher <lp_boucher@hotmail.com>
2020-02-14 17:48:44 +0100

    fix height of facebook plugin

--- src/components/Social/Facebookfeed.js
+++ src/components/Social/Facebookfeed.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FacebookFeed = ({query}) => {
     return (
         <div>
-            <iframe src={`https://www.facebook.com/plugins/page.php?href=${query}tabs=timeline&width=300&height=600&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=false&appId=193787871973148`} width="300" height="500" style={{border:'none',overflow:'hidden'}} scrolling="no" frameBorder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+            <iframe src={`https://www.facebook.com/plugins/page.php?href=${query}tabs=timeline&width=300&height=600&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=false&appId=193787871973148`} width="300" height="600" style={{border:'none',overflow:'hidden'}} scrolling="no" frameBorder="0" allowTransparency="true" allow="encrypted-media"></iframe>
         </div>
     );
 };
